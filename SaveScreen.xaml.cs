7b1611cd395e69335355e0faaf5ae785cab367bf
Graf Zahl <torpedokaefer69@gmx.de>
2021-02-25 18:05:16 +0100

    Finished_SaveLoad

--- SaveScreen.xaml.cs
+++ SaveScreen.xaml.cs
@@ -19,7 +19,8 @@ namespace DnD_CharSheet_5e
             InitializeComponent();
             fileManager.Find_RootPath();
             fileManager.Check_for_SaveGameFolder();
-            fileManager.Set_SaveGames();            
+            fileManager.Set_SaveGames();
+            Load_Names_for_SlotButtons();
         }
 
         public void Show_FilePath()
@@ -39,6 +40,7 @@ namespace DnD_CharSheet_5e
             saveGame_01_bt.Content = currenCharacter.Get_charName();
             SaveSystem.SaveCharacter(currenCharacter, fileManager.saveGame_01);
             SaveSystem.Save_CharName(currenCharacter.Get_charName(), fileManager.namesDataBase, 0);
+            this.Close();
         }
 
         public void SaveGame02_BT_Click(object sender, RoutedEventArgs e)
@@ -46,6 +48,7 @@ namespace DnD_CharSheet_5e
             saveGame_02_bt.Content = currenCharacter.Get_charName();
             SaveSystem.SaveCharacter(currenCharacter, fileManager.saveGame_02);
             SaveSystem.Save_CharName(currenCharacter.Get_charName(), fileManager.namesDataBase, 1);
+            this.Close();
         }
 
         public void SaveGame03_BT_Click(object sender, RoutedEventArgs e)
@@ -53,6 +56,7 @@ namespace DnD_CharSheet_5e
             saveGame_03_bt.Content = currenCharacter.Get_charName();
             SaveSystem.SaveCharacter(currenCharacter, fileManager.saveGame_03);
             SaveSystem.Save_CharName(currenCharacter.Get_charName(), fileManager.namesDataBase, 2);
+            this.Close();
         }
 
         public void SaveGame04_BT_Click(object sender, RoutedEventArgs e)
@@ -60,6 +64,7 @@ namespace DnD_CharSheet_5e
             saveGame_04_bt.Content = currenCharacter.Get_charName();
             SaveSystem.SaveCharacter(currenCharacter, fileManager.saveGame_04);
             SaveSystem.Save_CharName(currenCharacter.Get_charName(), fileManager.namesDataBase, 3);
+            this.Close();
         }
 
         public void SaveGame05_BT_Click(object sender, RoutedEventArgs e)
@@ -67,6 +72,21 @@ namespace DnD_CharSheet_5e
             saveGame_05_bt.Content = currenCharacter.Get_charName();
             SaveSystem.SaveCharacter(currenCharacter, fileManager.saveGame_05);
             SaveSystem.Save_CharName(currenCharacter.Get_charName(), fileManager.namesDataBase, 4);
+            this.Close();
+        }
+
+        public void Load_Names_for_SlotButtons()
+        {
+            characterNames = SaveSystem.Load_CharNames(fileManager.namesDataBase);
+
+            if(characterNames != null)
+            {
+                saveGame_01_bt.Content = characterNames[0];
+                saveGame_02_bt.Content = characterNames[1];
+                saveGame_03_bt.Content = characterNames[2];
+                saveGame_04_bt.Content = characterNames[3];
+                saveGame_05_bt.Content = characterNames[4];
+            }
         }
     }
 }
