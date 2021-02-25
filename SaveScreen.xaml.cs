f71ed7a6c118750c651ea7282b06bdd55cffd2eb
Graf Zahl <torpedokaefer69@gmx.de>
2021-02-25 11:07:13 +0100

    File Operations in Constructor
    also test for loading names

--- SaveScreen.xaml.cs
+++ SaveScreen.xaml.cs
@@ -12,10 +12,14 @@ namespace DnD_CharSheet_5e
     {
         FileManager fileManager = new FileManager();
         Character currenCharacter = new Character();
+        List<string> characterNames = new List<string>();
 
         public SaveScreen()
         {
             InitializeComponent();
+            fileManager.Find_RootPath();
+            fileManager.Check_for_SaveGameFolder();
+            fileManager.Set_SaveGames();            
         }
 
         public void Show_FilePath()
@@ -33,8 +37,36 @@ namespace DnD_CharSheet_5e
         public void SaveGame01_BT_Click(object sender, RoutedEventArgs e)
         {
             saveGame_01_bt.Content = currenCharacter.Get_charName();
-            SaveSystem.SaveCharacter(currenCharacter, fileManager.saveGame_01);            
-        }              
-       
+            SaveSystem.SaveCharacter(currenCharacter, fileManager.saveGame_01);
+            //SaveSystem.SaveName(currenCharacter.Get_charName(), fileManager.namesDataBase, 0, characterNames);
+        }
+
+        public void SaveGame02_BT_Click(object sender, RoutedEventArgs e)
+        {
+            saveGame_02_bt.Content = currenCharacter.Get_charName();
+            SaveSystem.SaveCharacter(currenCharacter, fileManager.saveGame_02);
+            //SaveSystem.SaveName(currenCharacter.Get_charName(), fileManager.namesDataBase, 1, characterNames);
+        }
+
+        public void SaveGame03_BT_Click(object sender, RoutedEventArgs e)
+        {
+            saveGame_03_bt.Content = currenCharacter.Get_charName();
+            SaveSystem.SaveCharacter(currenCharacter, fileManager.saveGame_03);
+            //SaveSystem.SaveName(currenCharacter.Get_charName(), fileManager.namesDataBase, 2, characterNames);
+        }
+
+        public void SaveGame04_BT_Click(object sender, RoutedEventArgs e)
+        {
+            saveGame_04_bt.Content = currenCharacter.Get_charName();
+            SaveSystem.SaveCharacter(currenCharacter, fileManager.saveGame_04);
+            //SaveSystem.SaveName(currenCharacter.Get_charName(), fileManager.namesDataBase, 3, characterNames);
+        }
+
+        public void SaveGame05_BT_Click(object sender, RoutedEventArgs e)
+        {
+            saveGame_05_bt.Content = currenCharacter.Get_charName();
+            SaveSystem.SaveCharacter(currenCharacter, fileManager.saveGame_05);
+            //SaveSystem.SaveName(currenCharacter.Get_charName(), fileManager.namesDataBase, 4, characterNames);
+        }
     }
 }
