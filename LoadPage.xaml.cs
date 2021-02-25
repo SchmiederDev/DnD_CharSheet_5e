77fd98524f0bb4ffeef2a35d572b7c613f1cc6e2
Graf Zahl <torpedokaefer69@gmx.de>
2021-02-25 18:04:36 +0100

    Deleted_fromProject

---
+++ LoadPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DnD_CharSheet_5e
+{
+    /// <summary>
+    /// Interaktionslogik für LoadPage.xaml
+    /// </summary>
+    public partial class LoadPage : Page
+    {
+        FileManager fileManager = new FileManager();
+        CharacterData L_charData = new CharacterData();
+        Character L_character = new Character();
+
+        bool charIsLoaded = false;
+        bool wasClosed = false;
+        public LoadPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Check_for_Files()
+        {
+            fileManager.Find_RootPath();
+            fileManager.Set_SaveGames();
+        }
+
+        public void LoadCharacter_Slot01bt_Click(object sender, RoutedEventArgs e)
+        {
+            L_charData = SaveSystem.LoadCharacter(fileManager.saveGame_01);
+            
+            if(L_charData != null)
+            {
+                L_character.Load_Character(L_charData);
+                charIsLoaded = true;
+            }            
+        }
+
+        public bool Get_CharLoad()
+        {
+            return charIsLoaded;
+        }
+
+        public Character Get_LoadedChar()
+        {
+            return L_character;
+        }        
+    }
+}
