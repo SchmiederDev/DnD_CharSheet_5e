ae9f33fbfa93801a93f2bd2bb856e1092d504947
Graf Zahl <torpedokaefer69@gmx.de>
2021-03-03 11:22:14 +0100

    Create Armor.cs

---
+++ Armor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DnD_CharSheet_5e
+{
+    public class Armor : Item
+    {
+        public uint ArmorBonus { get; set; }
+
+        public bool DexAdd { get; set; } = true;
+        public bool HasMax { get; set; } = false;
+        
+        public uint DexMax { get; } = 2;
+
+        public bool HasDisadvantage { get; set; }
+    }
+}
