f845d620e4d1de2422f330eb1620b6386bc0d483
Graf Zahl <torpedokaefer69@gmx.de>
2021-03-03 11:22:40 +0100

    Create Coin.cs

---
+++ Coin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DnD_CharSheet_5e
+{
+    public class Coin
+    {
+
+        public int Price { get; set; }
+        public string CoinKey { get; set;  }          
+        
+    }
+}
