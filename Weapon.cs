1a54189074835d73e5e72e21d3c55e4758307216
Graf Zahl <torpedokaefer69@gmx.de>
2021-03-03 11:25:35 +0100

    Create Weapon.cs

---
+++ Weapon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DnD_CharSheet_5e
+{
+    public class Weapon : Item
+    {
+        public uint DamageNominator { get; set; }
+        public uint DamageDenominator { get; set; }
+
+        public bool IsRanged { get; set; }
+        public bool IsFinesse { get; set; }
+    }
+}
