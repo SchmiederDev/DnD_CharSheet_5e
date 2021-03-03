e96ca151ebc19fda04ee469fa58c4dde9422e004
Graf Zahl <torpedokaefer69@gmx.de>
2021-03-03 11:23:47 +0100

    Create Item.cs

---
+++ Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DnD_CharSheet_5e
+{
+    public class Item
+    {
+        public string ItemName { get; set; }
+
+        public string Item_ID { get; set; }
+
+        public int ItemWeight { get; set; }
+
+        public Coin Coin;
+
+        public string ItemInfo { get; set; }
+    }
+}
