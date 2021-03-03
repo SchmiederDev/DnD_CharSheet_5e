e6c8af7401a6975e856a9873bb4397dd4463fdb4
Graf Zahl <torpedokaefer69@gmx.de>
2021-03-03 11:25:03 +0100

    Create Merchant.cs

---
+++ Merchant.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using Newtonsoft.Json;
+
+
+namespace DnD_CharSheet_5e
+{
+    public class Merchant
+    {
+        public FileManager fManager = new FileManager();
+        
+        public List<Item> merchItems = new List<Item>();
+
+        public void Load_ItemDataBase(string jsonIDB)
+        {            
+            merchItems = JsonConvert.DeserializeObject<List<Item>>(jsonIDB);            
+        }
+        
+        public Item Find_Item_byID(string id)
+        {
+            Item tempItem = new Item();
+
+            foreach(Item mItem in merchItems)
+            {
+                if(mItem.Item_ID == id)
+                {
+                    tempItem = mItem;
+                }
+            }
+
+            return tempItem;
+        }
+    }
+}
