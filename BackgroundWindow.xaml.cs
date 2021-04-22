e5b0878e3ae0995d89fc6ada78ff1eb559923247
Graf Zahl <torpedokaefer69@gmx.de>
2021-04-22 12:38:35 +0200

    Create BackgroundWindow.xaml.cs

---
+++ BackgroundWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace DnD_CharSheet_5e
+{
+    /// <summary>
+    /// Interaktionslogik für BackgroundWindow.xaml
+    /// </summary>
+    public partial class BackgroundWindow : Window
+    {
+        
+        public BackgroundWindow()
+        {
+            InitializeComponent();
+            Init_UI();
+        }
+
+        private void Init_UI()
+        {
+            CharacterName_Box.Text = SheetManager.CS_Manager_Inst.character.Get_charName();            
+            Load_Background();
+        }
+
+        private void Load_Background()
+        {
+            if(SheetManager.CS_Manager_Inst.character.CharApperance != null)
+            {
+                Appearance_Box.Text = SheetManager.CS_Manager_Inst.character.CharApperance;
+            }
+
+            if (SheetManager.CS_Manager_Inst.character.BackgroundStory != null)
+            {
+                Backstory_Box.Text = SheetManager.CS_Manager_Inst.character.BackgroundStory;
+            }
+
+            if (SheetManager.CS_Manager_Inst.character.AlliesAndOrgas != null)
+            {
+                Allies_n_Orgas_Box.Text = SheetManager.CS_Manager_Inst.character.AlliesAndOrgas;
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)                                                      // happens after Closing
+        {
+            
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)                         // happens before Closing
+        {
+            SheetManager.CS_Manager_Inst.character.CharApperance = Appearance_Box.Text;                             // <- Insert functionality that checks for text Limit
+            SheetManager.CS_Manager_Inst.character.BackgroundStory = Backstory_Box.Text;
+            SheetManager.CS_Manager_Inst.character.AlliesAndOrgas = Allies_n_Orgas_Box.Text;           
+        }
+    }
+}
