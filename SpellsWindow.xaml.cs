15351bef7fc5c0b24dbdac157b75d0176fa9ed35
Graf Zahl <torpedokaefer69@gmx.de>
2021-04-22 12:43:35 +0200

    Create SpellsWindow.xaml.cs

---
+++ SpellsWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DnD_CharSheet_5e
+{
+    /// <summary>
+    /// Interaktionslogik für SpellsWindow.xaml
+    /// </summary>
+    public partial class SpellsWindow : Window
+    {
+        public string[] Spellcaster_Classes { get; } = { "Bard", "Cleric", "Druid", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard" };
+        public SpellsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
