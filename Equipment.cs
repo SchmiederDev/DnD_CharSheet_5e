15240dd072351f6c7923b829844af3644a302d31
Graf Zahl <torpedokaefer69@gmx.de>
2021-05-21 06:10:26 +0200

    Create Equipment.cs

---
+++ Equipment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DnD_CharSheet_5e
+{
+    public class Equipment
+    {
+        public Armor CharacterArmor { get; set; }
+        public Armor LeftHand_Armor { get; set; }
+       
+        public Weapon RightHand_Weapon { get; set; }
+        public Weapon LeftHand_Weapon { get; set; }
+    }
+}
