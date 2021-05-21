795cd682e63ea2f822a6b9e356fff666f481991f
Graf Zahl <torpedokaefer69@gmx.de>
2021-05-21 07:47:09 +0200

    Update to Singleton FileManager

--- SaveScreen.xaml.cs
+++ SaveScreen.xaml.cs
@@ -9,25 +9,21 @@ namespace DnD_CharSheet_5e
     /// Interaktionslogik für SaveScreen.xaml
     /// </summary>
     public partial class SaveScreen : Window
-    {
-        FileManager fileManager = new FileManager();
+    {        
         Character currenCharacter = new Character();
         List<string> characterNames = new List<string>();
 
         public SaveScreen()
         {
-            InitializeComponent();
-            fileManager.Find_RootPath();
-            fileManager.Check_for_SaveGameFolder();
-            fileManager.Set_SaveGames();
+            InitializeComponent();                     
             Load_Names_for_SlotButtons();
         }
 
         public void Show_FilePath()
         {
-            pathVisualizer.Text = fileManager.Find_RootPath();
-            fileManager.Check_for_SaveGameFolder();
-            fileManager.Set_SaveGames();
+            pathVisualizer.Text = FileManager.FM_Inst.Find_RootPath();
+            FileManager.FM_Inst.Check_for_SaveGameFolder();
+            FileManager.FM_Inst.Set_SaveGames();
         }
 
         public void Fetch_Character(Character character)
@@ -38,46 +34,46 @@ namespace DnD_CharSheet_5e
         public void SaveGame01_BT_Click(object sender, RoutedEventArgs e)
         {
             saveGame_01_bt.Content = currenCharacter.Get_charName();
-            SaveSystem.SaveCharacter(currenCharacter, fileManager.saveGame_01);
-            SaveSystem.Save_CharName(currenCharacter.Get_charName(), fileManager.namesDataBase, 0);
+            SaveSystem.SaveCharacter(currenCharacter, FileManager.FM_Inst.saveGame_01);
+            SaveSystem.Save_CharName(currenCharacter.Get_charName(), FileManager.FM_Inst.namesDataBase, 0);
             this.Close();
         }
 
         public void SaveGame02_BT_Click(object sender, RoutedEventArgs e)
         {
             saveGame_02_bt.Content = currenCharacter.Get_charName();
-            SaveSystem.SaveCharacter(currenCharacter, fileManager.saveGame_02);
-            SaveSystem.Save_CharName(currenCharacter.Get_charName(), fileManager.namesDataBase, 1);
+            SaveSystem.SaveCharacter(currenCharacter, FileManager.FM_Inst.saveGame_02);
+            SaveSystem.Save_CharName(currenCharacter.Get_charName(), FileManager.FM_Inst.namesDataBase, 1);
             this.Close();
         }
 
         public void SaveGame03_BT_Click(object sender, RoutedEventArgs e)
         {
             saveGame_03_bt.Content = currenCharacter.Get_charName();
-            SaveSystem.SaveCharacter(currenCharacter, fileManager.saveGame_03);
-            SaveSystem.Save_CharName(currenCharacter.Get_charName(), fileManager.namesDataBase, 2);
+            SaveSystem.SaveCharacter(currenCharacter, FileManager.FM_Inst.saveGame_03);
+            SaveSystem.Save_CharName(currenCharacter.Get_charName(), FileManager.FM_Inst.namesDataBase, 2);
             this.Close();
         }
 
         public void SaveGame04_BT_Click(object sender, RoutedEventArgs e)
         {
             saveGame_04_bt.Content = currenCharacter.Get_charName();
-            SaveSystem.SaveCharacter(currenCharacter, fileManager.saveGame_04);
-            SaveSystem.Save_CharName(currenCharacter.Get_charName(), fileManager.namesDataBase, 3);
+            SaveSystem.SaveCharacter(currenCharacter, FileManager.FM_Inst.saveGame_04);
+            SaveSystem.Save_CharName(currenCharacter.Get_charName(), FileManager.FM_Inst.namesDataBase, 3);
             this.Close();
         }
 
         public void SaveGame05_BT_Click(object sender, RoutedEventArgs e)
         {
             saveGame_05_bt.Content = currenCharacter.Get_charName();
-            SaveSystem.SaveCharacter(currenCharacter, fileManager.saveGame_05);
-            SaveSystem.Save_CharName(currenCharacter.Get_charName(), fileManager.namesDataBase, 4);
+            SaveSystem.SaveCharacter(currenCharacter, FileManager.FM_Inst.saveGame_05);
+            SaveSystem.Save_CharName(currenCharacter.Get_charName(), FileManager.FM_Inst.namesDataBase, 4);
             this.Close();
         }
 
         public void Load_Names_for_SlotButtons()
         {
-            characterNames = SaveSystem.Load_CharNames(fileManager.namesDataBase);
+            characterNames = SaveSystem.Load_CharNames(FileManager.FM_Inst.namesDataBase);
 
             if(characterNames != null)
             {
