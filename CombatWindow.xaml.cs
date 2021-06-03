2924bffa12fd164bf765062da20332a8855cb725
Graf Zahl <torpedokaefer69@gmx.de>
2021-06-03 10:38:56 +0200

    Sounds

--- CombatWindow.xaml.cs
+++ CombatWindow.xaml.cs
@@ -39,51 +39,61 @@ namespace DnD_CharSheet_5e
         public void D4_Bt_Click(object sender, RoutedEventArgs e)
         {
             DiceMachine_Roll(CW_sheetManager.dSys.Roll_D4());
+            FileManager.FM_Inst.Play_DiceSound();
         }
 
         public void D6_Bt_Click(object sender, RoutedEventArgs e)
         {
             DiceMachine_Roll(CW_sheetManager.dSys.Roll_D6());
+            FileManager.FM_Inst.Play_DiceSound();
         }
 
         public void D8_Bt_Click(object sender, RoutedEventArgs e)
         {
             DiceMachine_Roll(CW_sheetManager.dSys.Roll_D8());
+            FileManager.FM_Inst.Play_DiceSound();
         }
 
         public void D10_Bt_Click(object sender, RoutedEventArgs e)
         {
             DiceMachine_Roll(CW_sheetManager.dSys.Roll_D10());
+            FileManager.FM_Inst.Play_DiceSound();
         }
 
         public void D12_Bt_Click(object sender, RoutedEventArgs e)
         {
             DiceMachine_Roll(CW_sheetManager.dSys.Roll_D12());
+            FileManager.FM_Inst.Play_DiceSound();
         }
 
         public void D20_Bt_Click(object sender, RoutedEventArgs e)
         {
             DiceMachine_Roll(CW_sheetManager.dSys.Roll_D20());
+            FileManager.FM_Inst.Play_DiceSound();
         }
 
         public void D100_Bt_Click(object sender, RoutedEventArgs e)
         {
             DiceMachine_Roll(CW_sheetManager.dSys.Roll_D100());
+            FileManager.FM_Inst.Play_DiceSound();
         }
 
         public void Melee_Attack_Click(object sender, RoutedEventArgs e)
         {
             Melee_Result.Text = CW_sheetManager.Melee_Attack().ToString();
+            FileManager.FM_Inst.Play_DiceSound();
         }
 
         public void Ranged_Attack_Click(object sender, RoutedEventArgs e)
         {
             Ranged_Result.Text = CW_sheetManager.Ranged_Attack().ToString();
+            FileManager.FM_Inst.Play_DiceSound();
         }
 
         public void Damage_Roll_Click(object sender, RoutedEventArgs e)
         {
             Damage_Result.Text = CW_sheetManager.Damage_Roll().ToString();
+            FileManager.FM_Inst.Play_DiceSound();
         }
 
         public void INT_Spell_Atk_Click(object sender, RoutedEventArgs e)
@@ -92,6 +102,7 @@ namespace DnD_CharSheet_5e
 
             result = CW_sheetManager.dSys.Roll_D20() + CW_sheetManager.character.Get_intModifier() + CW_sheetManager.character.Get_ProfBonus();
             Spell_Result.Text = result.ToString();
+            FileManager.FM_Inst.Play_DiceSound();
         }
 
         public void WIS_Spell_Atk_Click(object sender, RoutedEventArgs e)
@@ -100,6 +111,7 @@ namespace DnD_CharSheet_5e
 
             result = CW_sheetManager.dSys.Roll_D20() + CW_sheetManager.character.Get_wisModifier() + CW_sheetManager.character.Get_ProfBonus();
             Spell_Result.Text = result.ToString();
+            FileManager.FM_Inst.Play_DiceSound();
         }
 
         public void CHA_Spell_Atk_Click(object sender, RoutedEventArgs e)
@@ -108,6 +120,7 @@ namespace DnD_CharSheet_5e
 
             result = CW_sheetManager.dSys.Roll_D20() + CW_sheetManager.character.Get_chaModifier() + CW_sheetManager.character.Get_ProfBonus();
             Spell_Result.Text = result.ToString();
+            FileManager.FM_Inst.Play_DiceSound();
         }
 
         public void Add_STRMod_Bt_Click(object sender, RoutedEventArgs e)
