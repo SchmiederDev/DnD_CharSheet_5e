4f91c77fb89ec730739d7b7dc7818621271cf58e
Graf Zahl <torpedokaefer69@gmx.de>
2021-06-03 10:38:43 +0200

    Serializable

--- Coin.cs
+++ Coin.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 
 namespace DnD_CharSheet_5e
 {
+    [Serializable]
     public class Coin
     {
-
         public int Price { get; set; }
         public string CoinKey { get; set;  }          
         
