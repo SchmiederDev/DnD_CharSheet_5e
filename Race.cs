fea6a22858c9d18a16feba0696f2d2f99a1cd95c
Robert Schmieder <robert.schmieder87@gmx.de>
2022-03-04 10:15:43 +0100

    Create Race.cs

---
+++ Race.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DnD_CharSheet_5e
+{
+    public class Race
+    {
+        public string RaceName { get; set; }
+
+        // use class or struct instead? array/ list
+        public AbilityScoreIncrease[] AbilityScoreIncreases;
+        public uint AgeMax { get; set; }
+        public string AgeBackground { get; set; }
+
+        public string AlignmentBackground { get; set; }
+
+        public string Size { get; set; }
+        public string SizeBackground { get; set; }
+
+        public uint Speed { get; set; }
+
+        public RaceFeat[] RaceFeats { get; set; }
+
+        public Proficiency[] RaceProficiencies { get; set; }
+
+        public Race_SpellAbility[] Race_SpellAbilities { get; set; }
+
+        // At the moment only the languages spoken - not their respektive description - here with a key or own database?
+        public string[] Languages;
+
+        public Subrace[] SubracesOfRace { get; set; }
+
+    }
+
+    public struct RaceFeat
+    {
+        public string FeatName { get; set; }
+        public string FeatDescription { get; set; }
+    }
+
+    public struct Race_SpellAbility
+    {
+        public string SpellName {get; set;}
+        public uint ClassLevel { get; set; }
+    }
+
+    public struct AbilityScoreIncrease
+    {
+        public string AbilityKey;
+        public uint AbilityBonus;
+    }
+
+    public struct Height
+    {
+        public uint feet { get; set; }
+        public uint inches { get; set; }
+    }
+    
+
+    public class Subrace
+    {
+        public string SubraceName { get; set; }        
+
+        public AbilityScoreIncrease SubraceIncrease { get; set; }
+
+        public Height HeightMax { get; set; }
+        public Height HeightMin { get; set; }
+
+        public float WeightMax { get; set; }
+        public float WeightMin { get; set; }
+
+        public RaceFeat[] SubraceFeats { get; set; }
+
+        public Proficiency[] SubraceProficiencies { get; set; }
+
+        public Race_SpellAbility[] Subrace_SpellAbilities { get; set; }
+    }
+}
