8834f8a24ca5d3596ddf5b5541ada799398088f8
Robert Schmieder <robert.schmieder87@gmx.de>
2022-03-04 10:15:40 +0100

    Create Proficiency.cs

---
+++ Proficiency.cs
@@ -0,0 +1,9 @@
+﻿
+namespace DnD_CharSheet_5e
+{
+    public class Proficiency
+    {
+        public string ProficiencyType { get; set; }
+        public string ProficiencyName { get; set; }
+    }
+}
