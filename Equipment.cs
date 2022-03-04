68e1ee1f99f265723f981d4a148a5b4c62b884aa
Robert Schmieder <robert.schmieder87@gmx.de>
2022-03-04 10:10:20 +0100

    Update Equipment.cs
    Added more slots to equipment

--- Equipment.cs
+++ Equipment.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 namespace DnD_CharSheet_5e
 {
+    // Why again serializable?
     [Serializable]
     public class Equipment
     {
@@ -11,5 +12,15 @@ namespace DnD_CharSheet_5e
        
         public Weapon RightHand_Weapon { get; set; }
         public Weapon LeftHand_Weapon { get; set; }
+
+        public Item Head_Slot { get; set; }
+        public Item Neck_Slot { get; set; }
+        public Item Cape_Slot { get; set; }
+
+        public Item Belt_Slot { get; set; }
+        public Item Boots_Slot { get; set; }
+
+        public Item LeftHand_Ring { get; set; }
+        public Item RightHand_Ring { get; set; }
     }
 }
