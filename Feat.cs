84b3cafd76571fea73eb5ea8a7b904f5d3005f88
Robert Schmieder <robert.schmieder87@gmx.de>
2022-03-04 10:10:23 +0100

    Create Feat.cs

---
+++ Feat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DnD_CharSheet_5e
+{
+    public class Feat
+    {
+        // expand on this maybe later - since the customization options are not part of the open licence agreement - the original feats can't be integrated in a public version anyway
+        public string FeatName { get; set; }
+
+        public string FeatDescription { get; set; }
+
+    }
+}
