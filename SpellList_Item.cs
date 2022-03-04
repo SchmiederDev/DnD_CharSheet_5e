2b5a188ebf3ddaa4f6267d430bb2d61666ffec4b
Robert Schmieder <robert.schmieder87@gmx.de>
2022-03-04 10:19:25 +0100

    Create SpellList_Item.cs

---
+++ SpellList_Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DnD_CharSheet_5e
+{
+    public class SpellList_Item
+    {
+        public string SpellName { get; set; }
+        public int SpellLevel { get; set; }
+    }
+}
