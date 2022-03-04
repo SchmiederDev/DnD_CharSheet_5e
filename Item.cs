7dd9568c4e5ec1e2e57e24bddadca5ba9991d20d
Robert Schmieder <robert.schmieder87@gmx.de>
2022-03-04 10:12:20 +0100

    Update Item.cs
    added equipment feats

--- Item.cs
+++ Item.cs
@@ -7,11 +7,13 @@ namespace DnD_CharSheet_5e
     public class Item
     {
         public string ItemName { get; set; }
-
         public string Item_ID { get; set; }
 
-        public float ItemWeight { get; set; }
+        public bool IsEquipable { get; set; }
+        public bool IsConsumable { get; set; }
+        public string SlotReference { get; set; }
 
+        public float ItemWeight { get; set; }
         public Coin Coin;
 
         public string ItemInfo { get; set; }
