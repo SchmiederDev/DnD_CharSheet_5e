ac7deb7b78b81a8c19bb5720c38bff123fefb089
Robert Schmieder <robert.schmieder87@gmx.de>
2022-03-04 10:12:46 +0100

    Create MagicItem.cs

---
+++ MagicItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DnD_CharSheet_5e
+{
+    [Serializable]
+    public class MagicItem : Item
+    {
+        public string ItemTypeKey { get; set; }
+        public string ItemAttribute_ReferenceKey { get; set; }
+        public int ItemBonus { get; set; }
+
+        // Array?
+        public List<Spell> SpellsCastOnItem { get; set; }
+    }
+}
