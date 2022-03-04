2299add1701f45fd15c672ac55c71ffc1146cf50
Robert Schmieder <robert.schmieder87@gmx.de>
2022-03-04 10:06:44 +0100

    Create CharacterRace.cs

---
+++ CharacterRace.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DnD_CharSheet_5e
+{
+    public class CharacterRace
+    {
+        public Race RaceBackground { get; set; }
+        public Subrace CharakterSubrace { get; set; }
+
+        public Feat HumanFeat { get; set; }
+
+        public List<Proficiency> RaceProficiencies { get; set; }
+        public List<Race_SpellAbility> RaceSpellAbilities { get; set; }
+
+
+        public CharacterRace()
+        {
+            RaceBackground = new Race();
+            CharakterSubrace = new Subrace();
+
+            HumanFeat = new Feat();
+
+            RaceProficiencies = new List<Proficiency>();
+            RaceSpellAbilities = new List<Race_SpellAbility>();
+        }
+    }
+}
