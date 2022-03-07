ee7a502ac7d97b89746742ade6abba52f4bfda6c
Robert Schmieder <robert.schmieder87@gmx.de>
2022-03-07 10:20:52 +0100

    Update Armor.cs
    general int

--- Armor.cs
+++ Armor.cs
@@ -6,11 +6,11 @@ namespace DnD_CharSheet_5e
     [Serializable]
     public class Armor : Item
     {
-        public uint ArmorBonus { get; set; }
+        public int ArmorBonus { get; set; }
 
         public bool DexAdd { get; set; } = true;
         public bool HasMax { get; set; } = false;
         public bool HasDisadvantage { get; set; }
-        public uint StrMax { get; set; }
+        public int StrMax { get; set; }
     }
 }
