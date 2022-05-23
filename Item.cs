1477c4a429cc3d2e8b4f54d0cf783159c4837c55
Robert Schmieder <robert.schmieder87@gmx.de>
2022-05-23 13:10:03 +0200

    Update Item.cs
    Created default Item

--- Item.cs
+++ Item.cs
@@ -6,16 +6,18 @@ namespace DnD_CharSheet_5e
     [Serializable]
     public class Item
     {
-        public string ItemName { get; set; }
-        public string Item_ID { get; set; }
+        public string ItemName { get; set; } = "Default Item";
+        public string Item_ID { get; set; } = "i_00";
 
-        public bool IsEquipable { get; set; }
-        public bool IsConsumable { get; set; }
-        public string SlotReference { get; set; }
+        public bool IsEquipable { get; set; } = false;
+        public bool IsConsumable { get; set; } = false;
+        public string SlotReference { get; set; } = "none";
 
-        public float ItemWeight { get; set; }
+        public float ItemWeight { get; set; } = 0.0f;
+
         public Coin Coin;
 
-        public string ItemInfo { get; set; }
+        public string ItemInfo { get; set; } = "Place Holder Item";
+        
     }
 }
