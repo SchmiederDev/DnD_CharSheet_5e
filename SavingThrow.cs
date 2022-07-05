2a258e8eb474cf4000ed7d4ce5fb3bb05407681b
Robert Schmieder <robert.schmieder87@gmx.de>
2022-07-05 08:43:58 +0200

    Update Comments

--- SavingThrow.cs
+++ SavingThrow.cs
@@ -3,9 +3,27 @@ using System.Collections.Generic;
 
 namespace DnD_CharSheet_5e
 {
+    /* SAVING THROW CLASS:
+     * 
+     * Saving Throws are dice rolls made to check whether a character suffers from a harmful effect such as a lightning bolt hurdled towards them,
+     * the attempt of a creature to control their mind or if their are poisoned after drinking polluted water etc.
+     * 
+     * A 'Saving Throw' comes with a number of values determining how good a character is in fending off certain harmful effects.
+     * 
+     * This class is an abstract representation of these. It mainly stores the respective values, but can also calculate the modifier belonging to the 'Saving Throw'
+     * (= a number added or substracted from the result of the corresponding die roll).
+     * 
+     * Finally, it allows to make the die roll (= getting a random number in the range of the corresponding values) for the respective Saving Throw.
+     * 
+     * In the game there are 6 types of Saving Throws which are stored as properties of the 'Character'-class from which also the here defined methods are called.
+     * 
+     */
+
     [Serializable]
     public class SavingThrow
     {
+        #region PROPERTIES
+
         public string SaveName { get; set; }
         public string SaveKey { get; set; }
 
@@ -14,6 +32,10 @@ namespace DnD_CharSheet_5e
         public int SaveModifier { get; set; }
 
         public bool IsProficient { get; set; } = false;
+
+        #endregion
+
+        #region METHODS ->  Setting the Ability Bonus, calculating modifiers and returning random int for die roll 
 
         public void Set_AbilityBonus(int bonus)
         {
@@ -39,5 +61,7 @@ namespace DnD_CharSheet_5e
 
             return result;
         }
+
+        #endregion
     }
 }
