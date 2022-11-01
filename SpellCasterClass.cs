7c84877f3fae395a9d1bd5e4cba4d58393df1ff3
Robert Schmieder <robert.schmieder87@gmx.de>
2022-11-01 10:05:31 +0100

    Create SpellCasterClass.cs
    Class to store all relevant data/values to a type of character which is able to use magic in the game and does the respective calculations of some of the values needed in game -> will be integrated later as a member of a more complex character-class (D&D-'Class' = type of character)

---
+++ SpellCasterClass.cs
@@ -0,0 +1,32 @@
+﻿namespace DnD_CharSheet_5e
+{
+    public class SpellCasterClass
+    {
+        public string SCC_Name { set; get; }
+        public string SpellCastingAbility_Name { set; get; }
+        public string SpellCastingAbility_Key { set; get; }
+        public int SpellSaveDC { set; get; }
+        public int SpellAttackBonus { set; get; }
+        public int BaseModifierSaveDC { get;} = 8;
+        public int BaseModifierAtkBonus { get; } = 10;
+
+        public void Calculate_SpellSaveDC()
+        {
+            Ability tempSpellCastingAbility = SheetManager.CS_Manager_Inst.character.Abilities.Find(ability => ability.AbilityName == SpellCastingAbility_Name);
+            SpellSaveDC = BaseModifierSaveDC + tempSpellCastingAbility.Modifier + SheetManager.CS_Manager_Inst.character.ProficiencyBonus;
+        }
+
+        public void Calculate_SpellAttackBonus()
+        {
+            Ability tempSpellCastingAbility = SheetManager.CS_Manager_Inst.character.Abilities.Find(ability => ability.AbilityName == SpellCastingAbility_Name);
+            SpellAttackBonus = BaseModifierAtkBonus + tempSpellCastingAbility.Modifier + SheetManager.CS_Manager_Inst.character.ProficiencyBonus;
+        }
+
+        public void Generate_AbilityKey()
+        {
+            string tempAbilityKey = SpellCastingAbility_Name.Remove(3);
+            SpellCastingAbility_Key = tempAbilityKey.ToUpper();
+        }
+
+    }
+}
