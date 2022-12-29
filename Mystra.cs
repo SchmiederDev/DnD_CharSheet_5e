57dee06975593a84c6b1156a96b6733c2fc3771a
Robert Schmieder <robert.schmieder87@gmx.de>
2022-12-29 12:42:52 +0100

    Update Mystra.cs

--- Mystra.cs
+++ Mystra.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using System.Windows;
 
 namespace DnD_CharSheet_5e
 {
@@ -21,13 +22,37 @@ namespace DnD_CharSheet_5e
     {
         #region PROPERTIES AND CONSTRUCTOR
         public List<Spell> SpellDataBase { get; set; }
+
+        public List<SpellCasterData> SpellCasterClasses_Data {get; set;}
+        public List<SpellCasterClass> SpellCasterClasses { get; set; }
+
+        public List<SpellList> SpellLists { get; set; }
+
         public List<SpellList_Item> BardSpellList { get; set; }
+        public List<SpellList_Item> ClericSpellList { get; set; }
+        public List<SpellList_Item> DruidSpellList { get; set; }
+        public List<SpellList_Item> PaladinSpellList { get; set; }
+        public List<SpellList_Item> RangerSpellList { get; set; }
+        public List<SpellList_Item> SorcererSpellList { get; set; }
+        public List<SpellList_Item> WarlockSpellList { get; set; }
         public List<SpellList_Item> WizardSpellList { get; set; }
+
 
         public Mystra()
         {
             SpellDataBase = new List<Spell>();
+
+            SpellCasterClasses_Data = new List<SpellCasterData>();
+            SpellCasterClasses = new List<SpellCasterClass>();
+            SpellLists = new List<SpellList>();
+
             BardSpellList = new List<SpellList_Item>();
+            ClericSpellList = new List<SpellList_Item>();
+            DruidSpellList = new List<SpellList_Item>();
+            PaladinSpellList = new List<SpellList_Item>();
+            RangerSpellList = new List<SpellList_Item>();
+            SorcererSpellList = new List<SpellList_Item>();
+            WarlockSpellList = new List<SpellList_Item>();
             WizardSpellList = new List<SpellList_Item>();
         }
         #endregion
@@ -36,17 +61,81 @@ namespace DnD_CharSheet_5e
 
         public void Load_SpellDataBase(string jsonSDB)
         {
-            SpellDataBase = JsonConvert.DeserializeObject<List<Spell>>(jsonSDB);
+            try
+            {
+                SpellDataBase = JsonConvert.DeserializeObject<List<Spell>>(jsonSDB);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
 
-        public void Load_BardSpellList(string jsonBSL)
-        {
-            BardSpellList = JsonConvert.DeserializeObject<List<SpellList_Item>>(jsonBSL);
+        public void Load_SpellCasterClassesData(string jsonSCCDB)
+        {            
+            try
+            {
+                SpellCasterClasses_Data = JsonConvert.DeserializeObject<List<SpellCasterData>>(jsonSCCDB);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
 
-        public void Load_WizardSpellList(string jsonWSL)
+        public void Load_SpellListsDataBase(string jsonSLDB)
         {
-            WizardSpellList = JsonConvert.DeserializeObject<List<SpellList_Item>>(jsonWSL);
+            try
+            {
+                SpellLists = JsonConvert.DeserializeObject<List<SpellList>>(jsonSLDB);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+       
+        public void Load_SpellLists()
+        {
+            BardSpellList = Load_SpellList(FileManager.FM_Inst.BardSpellList_JSON);
+            ClericSpellList = Load_SpellList(FileManager.FM_Inst.ClericSpellList_JSON);
+            DruidSpellList = Load_SpellList(FileManager.FM_Inst.DruidSpellList_JSON);
+            PaladinSpellList = Load_SpellList(FileManager.FM_Inst.ClericSpellList_JSON);
+            RangerSpellList = Load_SpellList(FileManager.FM_Inst.RangerSpellList_JSON);
+            SorcererSpellList = Load_SpellList(FileManager.FM_Inst.SorcererSpellList_JSON);
+            WarlockSpellList = Load_SpellList(FileManager.FM_Inst.WarlockSpellList_JSON);
+            WizardSpellList = Load_SpellList(FileManager.FM_Inst.WizardSpellList_JSON);
+        }
+
+        public List<SpellList_Item> Load_SpellList(string jsonSpellList)
+        {
+            try
+            {
+                List<SpellList_Item> SpellList = JsonConvert.DeserializeObject<List<SpellList_Item>>(jsonSpellList);
+                return SpellList;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                return null;
+            }
+        }
+
+        #endregion
+
+        #region METHODS FOR HANDLING SPELL CASTER CLASSES
+
+        public void Initialize_SpellCasterClasses()
+        {
+            foreach(SpellCasterData SpellCasterData in SpellCasterClasses_Data)
+            {
+                SpellCasterClass CasterClass = new SpellCasterClass();
+                CasterClass.SCC_Name = SpellCasterData.SCC_Name;
+                CasterClass.SpellCastingAbility_Name = SpellCasterData.SC_Ability;
+                CasterClass.Generate_AbilityKey();
+                SpellCasterClasses.Add(CasterClass);
+            }
         }
 
         #endregion
