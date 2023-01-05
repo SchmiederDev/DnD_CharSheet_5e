14dedc24e142131d1dde1be81b83d7b4f6834deb
Robert Schmieder <robert.schmieder87@gmx.de>
2023-01-05 10:44:04 +0100

    Added disclaimer text about D&D licenses and copyright

--- GuideWdw.xaml.cs
+++ GuideWdw.xaml.cs
@@ -11,7 +11,7 @@ namespace DnD_CharSheet_5e
     public partial class GuideWdw : Window
     {
 
-        const string disclaimerTxtTxt = "TestText: Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+        const string disclaimerTxt = "The scope of the content from the original pen-and-paper game of D&D 5e  (D&D-rules, mechanics and background lore) this app works with extends to the information about the game published under the Open License Agreement.\nIt is meant to enhance the gaming experience of players and make their lives easier, but: for the full extent of game knowledge users will still have to own copies of the original core rule books (or additional source books) which contain also content which is not publicly accessible and falls under copyright owned by the official publisher Wizards of the Coast.";
 
         public GuideWdw()
         {
@@ -27,7 +27,7 @@ namespace DnD_CharSheet_5e
 
         private void InitDisclaimer()
         {
-            DisclaimerTxt.Text = disclaimerTxtTxt;
+            DisclaimerTxt.Text = disclaimerTxt;
         }
 
         private void InitCharGenTxt()
